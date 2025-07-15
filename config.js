a0d227649d32379b03c05aadd5510c6d051a3824
jesuscristoeosenhor <priorkaway@gmail.com>
2025-07-15 14:17:18 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  recreio: {
+    partner_api_key: "6q4ixnsf0wkpj88uyoob",
+    place_api_key: "UELJ8FL4",
+    telegram_token: "7622403986:AAE293hk8sDCc9Agzchavr-U7g8rBswPYe8",
+    chat_ids: ["1116351270"]
+  },
+  california: {
+    partner_api_key: "6q4ixnsf0wkpj88uyoob",
+    place_api_key: "AOUSECTP",
+    telegram_token: "8160838271:AAFPpQuOgyGaNsfA9tmlyGMCCYTiFXoIRGY",
+    chat_ids: ["1116351270"]
+  }
+};
